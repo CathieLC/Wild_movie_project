c51a7d7c2c14ab1b3dc5ab787e51191d666493b9
Guillaume Arp <guillaume.arp@gmail.com>
2021-11-05 08:58:45 +0100

    Edited streamlit page config

--- Guillaume/streamlit_project.py
+++ Guillaume/streamlit_project.py
@@ -4,6 +4,8 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go 
 
+st.set_page_config(page_title='Movie Analysis', page_icon=':shark:')
+
 st.title('Movie Analysis Project')
 
 st.subheader('Movie Duration Evolution')
