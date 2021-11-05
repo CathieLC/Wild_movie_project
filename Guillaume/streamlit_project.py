33302127615afb3d8b3d1f30368812edb901b013
Guillaume Arp <guillaume.arp@gmail.com>
2021-11-05 15:33:20 +0100

    Update streamlit_project.py
    Added a sidebar and multipage support

--- Guillaume/streamlit_project.py
+++ Guillaume/streamlit_project.py
@@ -4,10 +4,10 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go 
 
-st.set_page_config(page_title='Movie Analysis', page_icon=':shark:')
+st.set_page_config(page_title='Movie Analysis', page_icon=':cat:')
 
 def _max_width_():
-    max_width_str = f"max-width: 1300px;"
+    max_width_str = "max-width: 1300px;"
     st.markdown(
         f"""
     <style>
@@ -22,6 +22,11 @@ def _max_width_():
 _max_width_()
 
 st.title('Movie Analysis Project')
+
+# add_selectbox = st.sidebar.selectbox(
+#     "Select a KPI (test, does nothing yet)",
+#     ("Movie Duration", "Ratings")
+# )
 
 @st.cache
 def load_basics():
@@ -62,61 +67,103 @@ data_ratings = load_ratings()
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
-st.subheader('Movie Duration Evolution')
 
-'We first wanted to analyze the evolution of the average duration of movies over the years.'
-'We filtered the type to movies only, and removed the adult ones. Finally, the movies were grouped by their release year and the duration averaged for each year.'
-'We decided to only take into account the movies released during or after 1918, due to the lack of consistent data before this time, and the more experimental nature of the film industry.'
-'Finally, we removed the outliers regarding movie duration by setting the minimum duration to 58 minutes (the minimum to qualify as a feature film), and the maximum duration to 270 minutes.'
 
-fig = px.line(
-    data_runtime,
-    y='runtimeMinutes',
-    title='Average movie duration per year',
-    line_shape='spline',
-    labels={'startYear': 'Year', 'runtimeMinutes': 'Movie Duration in Minutes'},
-    color_discrete_sequence=['green'],
-    template='plotly_dark'
+def main():
+
+    pages = {
+        'Home': home,
+        'Movie Duration': movie_duration,
+        'Ratings': movie_ratings}
+
+    if "page" not in st.session_state:
+        st.session_state.update({
+        # Default page
+        'page': 'Home'
+        })
+
+    with st.sidebar:
+        page = st.selectbox("Choose a page", tuple(pages.keys()))
+
+    pages[page]()
+
+
+def home():
+
+    st.subheader('About this project')
+
+    'The purpose of this project is to help a new movie theatre owner to get a better idea what the film industry looks like at the moment, and how it has evolved over the years.'
+    'Then, we will provide a recommendation algorithm to help him select the best movies to play at the theatre, based on the tastes of his audience.'
+    ''
+    'This app is brought to you by the awesome Wild Data Green Team 1 (yes we need a better name):'
+    '   - Guillaume Arp'
+    '   - Franck Joly'
+    '   - Catherine Le Calve'
+    '   - Josse Preis'
+
+def movie_duration():
+    
+    st.subheader('Movie Duration Evolution')
+
+    'We first wanted to analyze the evolution of the average duration of movies over the years.'
+    'We filtered the type to movies only, and removed the adult ones. Finally, the movies were grouped by their release year and the duration averaged for each year.'
+    'We decided to only take into account the movies released during or after 1918, due to the lack of consistent data before this time, and the more experimental nature of the film industry.'
+    'Finally, we removed the outliers regarding movie duration by setting the minimum duration to 58 minutes (the minimum to qualify as a feature film), and the maximum duration to 270 minutes.'
+
+    fig = px.line(
+        data_runtime,
+        y='runtimeMinutes',
+        title='Average movie duration per year',
+        line_shape='spline',
+        labels={'startYear': 'Year', 'runtimeMinutes': 'Movie Duration in Minutes'},
+        color_discrete_sequence=['green'],
+        template='plotly_dark'
+        )
+
+    fig.update_layout(height=600)
+    st.plotly_chart(fig, use_container_width=True)
+
+    'We can notice here that the average movie duration has been steadily increasing until the early 60s, and then has been somewhat stable since then, around 95 minutes.'
+
+def movie_ratings():
+
+    st.subheader('Top Rated Movies')
+
+    'Another point we wanted to look into regarded the best rated movies in the database.'
+    'We filtered the movies to only include movies with an average rating of 8.4 or higher, and a minimum number of votes of 20000. That leaves us with a list of 109 movies, split by genre. When multiple genres were specified in the movie description, we only kept the first one, which is considered to be the main genre of the movie.'
+    'The filter of 20000 votes is there to make sure that the selected movies are indeed considered to be very good by a large and diverse audience.'
+    'First we can use a 3D Scatter Plot to see which movies are best rated, by year, genre and number of votes.'
+
+    fig = px.scatter_3d(data_ratings,
+        x='startYear',
+        y='averageRating',
+        z='mainGenre',
+        color='averageRating',
+        size='numVotes',
+        opacity = 0.8,
+        labels={
+            'startYear': 'Year',
+            'averageRating': 'Rating',
+            'mainGenre': 'Genre',
+            'numVotes': 'Number of Votes'
+        },
+        size_max=25,
+        template='plotly_dark',
+        hover_name='primaryTitle'
     )
 
-st.plotly_chart(fig, use_container_width=True)
+    fig.update_layout(height=800, scene=dict(zaxis=dict(nticks=11)), title='IMDB Top Rated Movies (>= 8.4) per Genre, Number of Votes and Year')
+    st.plotly_chart(fig, use_container_width=True)
 
-'We can notice here that the average movie duration has been steadily increasing until the early 60s, and then has been somewhat stable since then, around 95 minutes.'
+    'The mouseover shows the title of the movie, and the size of the bubble represents the number of votes. That way, it is easier to have a clear view of which movies are best rated, and by how many people.'
+    "Let's have a look now at a histogram showing more specifically how many movies in that list belong to each genre."
 
-st.subheader('Top Rated Movies')
+    fig = px.histogram(data_frame=data_ratings, x='mainGenre', color='mainGenre', labels={'mainGenre': 'Genre'}, color_discrete_sequence=px.colors.qualitative.Pastel)
 
-'Another point we wanted to look into regarded the best rated movies in the database.'
-'We filtered the movies to only include movies with an average rating of 8.4 or higher, and a minimum number of votes of 20000. That leaves us with a list of 109 movies, split by genre. When multiple genres were specified in the movie description, we only kept the first one, which is considered to be the main genre of the movie.'
-'The filter of 20000 votes is there to make sure that the selected movies are indeed considered to be very good by a large and diverse audience.'
-'First we can use a 3D Scatter Plot to see which movies are best rated, by year, genre and number of votes.'
+    fig.update_layout(height=600, title='IMDB Top Rated Movies (>= 8.4) Genre Distribution', template='plotly_dark')
+    st.plotly_chart(fig, use_container_width=True)
 
-fig = px.scatter_3d(data_ratings, 
-    x='startYear', 
-    y='averageRating', 
-    z='mainGenre', 
-    color='averageRating', 
-    size='numVotes', 
-    opacity = 0.8,
-    labels={
-        'startYear': 'Year',
-        'averageRating': 'Rating',
-        'mainGenre': 'Genre',
-        'numVotes': 'Number of Votes'
-    }, 
-    size_max=25,
-    template='plotly_dark',
-    hover_name='primaryTitle'
-)
+    'As we can notice here, almost half of all the movies in the list are dramas or action movies.'
 
-fig.update_layout(height=800, scene=dict(zaxis=dict(nticks=11)), title='IMDB Top Rated Movies (>= 8.4) per Genre, Number of Votes and Year')
-st.plotly_chart(fig, use_container_width=True)
-
-'The mouseover shows the title of the movie, and the size of the bubble represents the number of votes. That way, it is easier to have a clear view of which movies are best rated, and by how many people.'
-"Let's have a look now at a histogram showing more specifically how many movies in that list belong to each genre."
-
-fig = px.histogram(data_frame=data_ratings, x='mainGenre', color='mainGenre', labels={'mainGenre': 'Genre'}, color_discrete_sequence=px.colors.qualitative.Pastel)
-
-fig.update_layout(height=600, title='IMDB Top Rated Movies (>= 8.4) Genre Distribution', template='plotly_dark')
-st.plotly_chart(fig, use_container_width=True)
-
-'As we can notice here, almost half of all the movies in the list are dramas or action movies.'
+if __name__ == "__main__":
+    main()
