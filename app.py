ad7f751705e763d6af7a23d2b43c643e085d9433
Guillaume Arp <guillaume.arp@gmail.com>
2021-11-17 17:40:37 +0100

    Updated cleaning and reqs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from sklearn.cluster import KMeans
 
 st.set_page_config(page_title='Movie Analysis', page_icon=':cat:')
 
